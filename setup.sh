7ac6d24484758daaec84c50f45a8836ce68dfbb1
Trung Ngo <trungnqcs2000@gmail.com>
2024-04-18 03:10:50 -0700

    setup script for dowloading necessary datasets

--- setup.sh
+++ setup.sh
@@ -8,7 +8,8 @@ kaggle competitions download -c llm-detect-ai-generated-text
 unzip llm-detect-ai-generated-text.zip -d llm-detect-ai-generated-text
 rm llm-detect-ai-generated-text.zip
 
+mkdir persuade_2
 kaggle datasets download -d nbroad/persaude-corpus-2
-unzip persaude-corpus-2.zip -d ./
+unzip persaude-corpus-2.zip -d ./persuade_2
 rm persaude-corpus-2.zip
 
